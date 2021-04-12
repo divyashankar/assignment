a60992de2472ff1397159ed3cd351aef7ee06da9
Divya Shankar <69165144+divyashankar@users.noreply.github.com>
2021-04-12 18:55:28 +0530

    Add files via upload
    2nd Assignment (exc. edit and delete functionality)

--- src/App.js
+++ src/App.js
@@ -1,64 +1,95 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import {useState} from 'react';
+import {useState , useEffect} from 'react';
 import Customers from './components/Customers';
 import './App.css';
 import CustomerForm from './components/CustomerForm';
 
 function App() {
-  const deleteCust = (customerId) =>{
-    console.log("Deleted Customer with ID : "+customerId)
+  const [customers,setCustomers] = useState([]);
+  const [inEdit,setEdit] = useState(false);
+  const [custById,setCustById] = useState(null);
+
+  const getCustomersfromDB = async() =>{
+    const res = await fetch("http://localhost:5000/customers/");
+    const data = await res.json();
+    return data; 
+  };
+
+
+  const fetchCustomerById = async(customerId)=> { 
+    const res = await fetch("http://localhost:5000/customers/"+customerId);
+    const data = res.json();
+    return data; 
+   };
+
+  useEffect(()=>{
+    const customerData = async() =>{
+      const data = await getCustomersfromDB();
+      setCustomers(data);
+    };
+    customerData(); 
+  });
+
+
+  const deleteCust = async(id) =>{
+    const res = await fetch("http://localhost:5000/customers/"+id,{
+      method : 'DELETE'
+    })
+    const data = await res.json()
+    setCustomers([...customers,data])
   }
 
   const editCust = (customerId) => {
-    console.log("Edited Customer with ID : "+customerId)
-    {<CustomerForm/>}
+    setEdit(true);
+    setCustById(getCustomerById(customerId));
+
   }
 
-  const addCustomer = (customer)=>{
-    const id = Math.random()
-    const newCustomer = {id,...customer}
-    setCustomers([...customers,newCustomer])
+  const addCustomer = async (customer,isEdit)=>{
+    if(!isEdit){
+      const res = await fetch("http://localhost:5000/customers/",{
+        method : 'POST',
+        body : JSON.stringify(customer) 
+      })
+      const data = await res.json()
+      setCustomers([...customers,data])    
+    }
+    else{
+      const editedCustObj = {
+        id : customer.customerId,
+        customerName : customer.customerName,
+        customerLocation : customer.customerName
+      }
+      const res = await fetch("http://localhost:5000/customers/"+customer.customerId,{
+        method : 'PUT',
+        body : JSON.stringify(editedCustObj) 
+      })
+      const data = await res.json()
+      setCustomers([...customers,data])
+      setEdit(false) 
+    }
+  };
+
+ const getCustomerById = async(customerId)=> { 
+    let cust; 
+    customers.forEach((item)=>{
+      if(item.customerId === customerId){
+        cust = item; 
+      }
+    })
+   return cust; 
   }
 
-  const [showAddCustomer , setShowAddCustomer] = useState(false)
-
-  const [customers , setCustomers] = useState([
-    {	
-      "customerId" : "01",
-      "customerName" : "Jawahar",
-      "customerLocation" : "India" 
-    }, 
-    {	
-      "customerId" : "02",
-      "customerName" : "Subash",
-      "customerLocation" : "India" 
-    }, 
-    {	
-      "customerId" : "03",
-      "customerName" : "Bhagat",
-      "customerLocation" : "India" 
-    }, 
-    {	
-      "customerId" : "04",
-      "customerName" : "Tilak",
-      "customerLocation" : "India" 
-    }, 
-    {	
-      "customerId" : "05",
-      "customerName" : "Shastri",
-      "customerLocation" : "India" 
-    }, 
-    
-  ])
   return (
     <div className="App">
-      <Header title="CUSTOMER INFORMATION"
-      onAdd={()=> setShowAddCustomer(!showAddCustomer)}
-      showAdd={showAddCustomer} />
-      {showAddCustomer && <CustomerForm onAdd={addCustomer}/>}
+      <Header title="CUSTOMER INFORMATION"/>
+      <CustomerForm
+      addCustomer = {addCustomer}
+      inEditState = {inEdit}
+      custById = {custById}/>
       {customers.length>0 ? <Customers customers = {customers} 
-      onDelete = {deleteCust} onEdit={editCust}></Customers> : "No customer information to display"}
+      deleteCustomer = {deleteCust} editCustomer={editCust}></Customers> : "No customer information to display"}
       <Footer></Footer>
     </div>
   );
